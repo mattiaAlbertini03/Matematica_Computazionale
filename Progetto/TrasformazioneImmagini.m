cb2697904a920de4189eed8233d836d15466ae73
Axelredx <a109ros@gmail.com>
2026-04-12 14:21:36 +0200

    aggiunto generatore di immagini di mammiferi in base al seed dato

--- Progetto/TrasformazioneImmagini.m
+++ Progetto/TrasformazioneImmagini.m
@@ -10,10 +10,10 @@
 NotebookFileLineBreakTest
 NotebookFileLineBreakTest
 NotebookDataPosition[       154,          7]
-NotebookDataLength[      6895,        183]
-NotebookOptionsPosition[      6326,        165]
-NotebookOutlinePosition[      6717,        181]
-CellTagsIndexPosition[      6674,        178]
+NotebookDataLength[      9422,        238]
+NotebookOptionsPosition[      8852,        220]
+NotebookOutlinePosition[      9243,        236]
+CellTagsIndexPosition[      9200,        233]
 WindowFrame->Normal*)
 
 (* Beginning of Notebook Content *)
@@ -90,11 +90,62 @@ Cell[BoxData[
      "=", "\"\<Funzione che ritorna TRUE se le due immagini messe a confronto \
 sono uguali, FALSE altrimenti\>\""}], ";"}], "\[IndentingNewLine]", "\t", 
    RowBox[{
-    RowBox[{"Begin", "[", "\"\<Private`\>\"", "]"}], ";"}], "\[IndentingNewLine]",
-    "\t\t", 
+    RowBox[{"Begin", "[", "\"\<Private`\>\"", "]"}], ";"}], "\[IndentingNewLine]", 
+   RowBox[{
+    RowBox[{"imgageFromSeed", "::", "usage"}], 
+    "=", "\"\<Funzione che ritorna un'immagine specifica in base al seed \
+numerico di input\>\""}], "\[IndentingNewLine]", "\t\t", 
    RowBox[{
     RowBox[{"x", " ", "=", "3"}], ";"}], "\[IndentingNewLine]", "\t", 
    RowBox[{"(*", "Codice", "*)"}], "\[IndentingNewLine]", 
+   "\[IndentingNewLine]", "\[IndentingNewLine]", 
+   RowBox[{"(*", 
+    RowBox[{"La", " ", 
+     RowBox[{"varabile", " ", "'"}], 
+     RowBox[{"animali", "'"}], " ", "funge", " ", "da", " ", 
+     RowBox[{"database", "/", "lista"}], " ", "locale", " ", "di", " ", 
+     "foto", " ", "per", " ", "50", " ", "specie", " ", "di", " ", 
+     "mammiferi", " ", "differenti"}], "*)"}], "\[IndentingNewLine]", 
+   RowBox[{
+    RowBox[{"animali", "=", 
+     RowBox[{
+      RowBox[{"EntityList", "[", 
+       RowBox[{"EntityClass", "[", 
+        RowBox[{"\"\<Species\>\"", ",", "\"\<MammalSpecies\>\""}], "]"}], 
+       "]"}], "[", 
+      RowBox[{"[", 
+       RowBox[{"1", ";;", "50"}], "]"}], "]"}]}], ";"}], "\n", 
+   "\[IndentingNewLine]", 
+   RowBox[{"(*", 
+    RowBox[{"Funzione", " ", "che", " ", "ritorna", " ", 
+     RowBox[{"un", "'"}], "immagine", " ", "specifica", " ", "in", " ", 
+     "base", " ", "al", " ", "seed", " ", "numerico", " ", "dato", " ", "in", 
+     " ", "input"}], "*)"}], "\[IndentingNewLine]", 
+   RowBox[{
+    RowBox[{"imageFromSeed", "[", "seed_Integer", "]"}], ":=", 
+    RowBox[{"Module", "[", 
+     RowBox[{
+      RowBox[{"{", "indice", "}"}], ",", "\[IndentingNewLine]", 
+      RowBox[{"(*", 
+       RowBox[{
+        RowBox[{"Si", " ", "ricava", " ", 
+         RowBox[{"l", "'"}], "indice", " ", "viene", " ", "ricavato", " ", 
+         "tramite", " ", "operazione", " ", "MODULO", " ", "e", " ", 
+         "addizionato", " ", "1", " ", "per", " ", "avere", " ", "sempre", " ",
+          "un", " ", "indice", " ", "valido"}], ",", " ", 
+        RowBox[{"ovvero", " ", ">=", "1"}]}], "*)"}], "\[IndentingNewLine]", 
+      RowBox[{
+       RowBox[{"indice", "=", 
+        RowBox[{
+         RowBox[{"Mod", "[", 
+          RowBox[{"seed", ",", 
+           RowBox[{"Length", "[", "animali", "]"}]}], "]"}], "+", "1"}]}], ";",
+        "\[IndentingNewLine]", 
+       RowBox[{"EntityValue", "[", 
+        RowBox[{
+         RowBox[{"animali", "[", 
+          RowBox[{"[", "indice", "]"}], "]"}], ",", "\"\<Image\>\""}], 
+        "]"}]}]}], "\[IndentingNewLine]", "]"}]}], "\[IndentingNewLine]", 
    "\[IndentingNewLine]", 
    RowBox[{"(*", 
     RowBox[{
@@ -147,8 +198,12 @@ sono uguali, FALSE altrimenti\>\""}], ";"}], "\[IndentingNewLine]", "\t",
    3.9848135597958813`*^9, 3.9848135886680984`*^9}, {3.9848138071813087`*^9, 
    3.984813898658329*^9}, 3.984817755385786*^9, {3.984978428333962*^9, 
    3.984978545846283*^9}, {3.984978941461216*^9, 3.9849789865614967`*^9}, 
-   3.98497956719174*^9},
- CellLabel->"In[35]:=",ExpressionUUID->"83c80bd8-0237-de4f-8b6a-d9776d21b4fc"],
+   3.98497956719174*^9, {3.984984488879463*^9, 3.9849847662820377`*^9}, {
+   3.984984844424203*^9, 3.9849848455768957`*^9}, {3.984984882437913*^9, 
+   3.984984906406342*^9}, {3.984984999495994*^9, 3.984985002013451*^9}, {
+   3.9849850827446537`*^9, 
+   3.9849851641524887`*^9}},ExpressionUUID->"83c80bd8-0237-de4f-8b6a-\
+d9776d21b4fc"],
 
 Cell[CellGroupData[{
 
@@ -180,10 +235,10 @@ CellTagsIndex->{}
 *)
 (*NotebookFileOutline
 Notebook[{
-Cell[554, 20, 5362, 130, 917, "Input",ExpressionUUID->"83c80bd8-0237-de4f-8b6a-d9776d21b4fc"],
+Cell[554, 20, 7888, 185, 1234, "Input",ExpressionUUID->"83c80bd8-0237-de4f-8b6a-d9776d21b4fc"],
 Cell[CellGroupData[{
-Cell[5941, 154, 188, 3, 29, "Input",ExpressionUUID->"0e34f1bf-b31d-4669-abe4-b92f868e4d9b"],
-Cell[6132, 159, 178, 3, 33, "Output",ExpressionUUID->"66462ef5-7282-4a03-803e-560d66b78db8"]
+Cell[8467, 209, 188, 3, 29, "Input",ExpressionUUID->"0e34f1bf-b31d-4669-abe4-b92f868e4d9b"],
+Cell[8658, 214, 178, 3, 33, "Output",ExpressionUUID->"66462ef5-7282-4a03-803e-560d66b78db8"]
 }, Open  ]]
 }
 ]
